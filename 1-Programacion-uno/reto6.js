915e559c1c5eaaec5308ca8f50cef49b04f36c21
Alejandro Mora <Administrador@AD-GERTG.GPAMED.LOCAL>
2020-08-23 16:28:57 -0500

    subida de mas ejercicios

---
+++ 1-Programacion-uno/reto6.js
@@ -0,0 +1,5 @@
+const body=document.querySelector('#Contenido');
+
+const titulo = '<h1>Lorem ipsum</h1> <img src="http://via.placeholder.com/350x150" alt="imagen"/> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+
+body.innerHTML=titulo;
